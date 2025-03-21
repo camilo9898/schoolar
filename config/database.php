df88123929dfb99e819537288c1e89c35da9af68
camilo <cclopez.5400@unicesmag.edu.co>
2025-03-21 18:03:22 -0500

    schoolar

---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+    //Config connection
+    $host       = "localhost";
+    $port       = "5432";
+    $dbname     = "schoolar";
+    $user       = "postgres";
+    $password   = "unicesmag";
+
+    //Create connection
+    $conn = pg_connect("
+        host = $host
+        port = $port
+        dbname = $dbname
+        user = $user
+        password =$password
+    ");
+
+    if(!$conn){
+        
+        die("Connection error: " . preg_last_error());
+
+    }else{
+        
+        echo "Success connection";
+
+    }
+
+?>
