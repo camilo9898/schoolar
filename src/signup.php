8d3436685516258beab93532d83fdf8a34e5c675
camilo <cclopez.5400@unicesmag.edu.co>
2025-03-27 11:29:20 -0500

    post

---
+++ src/signup.php
@@ -0,0 +1,25 @@
+<?php
+
+include('../config/database.php');
+
+$fname      = $_POST['f_name'];
+$lname      = $_POST['l_name'];
+$email      = $_POST['e_mail'];
+$passwd     = $_POST['passw'];
+
+$sql = "INSERT INTO users (firstname, lastname, email, password)
+
+         VALUES('$fname','$lname','$email','$passwd')
+
+";
+
+ $res = pg_query($conn, $sql);
+
+ if ($res){
+     echo "USER HAS BEEN CREATED SUCCESFULLY";
+ }else{
+    
+    echo "Error";
+
+ }
+?>
