a6573cb599e53fa0d1ce7b05fcfe0030016fec9b
camilo <cclopez.5400@unicesmag.edu.co>
2025-03-28 18:11:55 -0500

    email Validation

--- src/signup.php
+++ src/signup.php
@@ -7,9 +7,26 @@ $lname      = $_POST['l_name'];
 $email      = $_POST['e_mail'];
 $passwd     = $_POST['passw'];
 
-$sql = "INSERT INTO users (firstname, lastname, email, password)
+//$enc_pass = md5($passwd);
+$enc_pass = sha1($passwd);
 
-         VALUES('$fname','$lname','$email','$passwd')
+$sql_email_exist = "SELECT COUNT(email) as total FROM users WHERE email = '$email' Limit 1";
+
+$res = pg_query($conn, $sql_email_exist);
+
+if($res){
+
+    $row = pg_fetch_assoc($res);
+
+    if($row['total'] > 0){
+
+        echo "Email already exists";
+        
+    }else{
+
+      $sql = "INSERT INTO users (firstname, lastname, email, password)
+
+         VALUES('$fname','$lname','$email','$enc_pass')
 
 ";
 
@@ -19,7 +36,8 @@ $sql = "INSERT INTO users (firstname, lastname, email, password)
      echo "USER HAS BEEN CREATED SUCCESFULLY";
  }else{
     
-    echo "Error";
-
+    echo "Error";  
+    }
+    }
  }
 ?>
