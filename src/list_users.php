49a6692ec0e57cb5b7523ee69074abebd5e896bb
camilo <cclopez.5400@unicesmag.edu.co>
2025-05-23 18:02:34 -0500

    photo

--- src/list_users.php
+++ src/list_users.php
@@ -16,6 +16,7 @@ include('../config/database.php');
         <th>Lastname</th>
         <th>E-mail</th>
         <th>Status</th>
+        <th>Photo</th>
         <th>...</th>
     </tr>
         
@@ -24,6 +25,7 @@ include('../config/database.php');
         //Here code
         $sql = "
         select
+        id,
         firstname,
         lastname,
         email,
@@ -42,10 +44,11 @@ from
             echo "<td>". $row ['lastname'] ."</td>";
             echo "<td>". $row ['email'] ."</td>";
             echo "<td>". $row ['status'] ."</td>";
+            echo "<td aling= 'center'><img src='photo_users/users-vector-icon-png_260862.jpg' width ='30'></td>";
             echo "<td>"; 
             echo "<a href=''><img src= 'icons/edit.png' width ='10'></a>";
             echo "<a href=''><img src= 'icons/basura.png' width ='15'></a>";
-            echo "<a href=''><img src= 'icons/buscar.png' width ='15'></a>";
+            echo "<a href='http://127.0.0.1/scholar/src/delete.php'><img src= 'icons/buscar.png' width ='15'></a>";
             echo "</td>";
             echo "</tr>";  
         }
